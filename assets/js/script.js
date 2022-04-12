034b941c8dd5b366e1eb449e768156492bd271de
Laura Antunez <lauraantunez024@gmail.com>
2022-04-11 20:29:46 -0400

    included real estate api call

--- assets/js/script.js
+++ assets/js/script.js
@@ -15,31 +15,49 @@ form.addEventListener("submit", function(event) {
 
 
 
+
+
+
+
+
+
+const settings = {
+	"async": true,
+	"crossDomain": true,
+	"url": "https://us-real-estate.p.rapidapi.com/location/for-sale-nearby-areas-by-postal-code?postal_code=10022",
+	"method": "GET",
+	"headers": {
+		"X-RapidAPI-Host": "us-real-estate.p.rapidapi.com",
+		"X-RapidAPI-Key": "395cc9c100mshac427d1df6a23e9p130807jsn016fa6b52c1c"
+	}
+};
+
+$.ajax(settings).done(function (response) {
+	console.log(response);
+});
+
+
 ///Fernando's API Call
 
 
-$(document).ready(function () {
-    var apiKey = "I28sS2O89AHgGz3gUm9lZBAXNk2HwB0N";
-    var baseUrl = "api.tomtom.com"
-    var versionNumber = 4;
-    var style = "absolute";
-    var zoom = 10;
-    var format = "json";
-    var point = "33.03,-93.15";
-    var urlKey = `https://${baseUrl}/traffic/services/${versionNumber}/flowSegmentData/${style}/${zoom}/${format}?key=${apiKey}&point=${point}`//&unit={unit}&thickness={thickness}&openLr={boolean}&jsonp={jsonp}
+// $(document).ready(function () {
+//     var apiKey = "I28sS2O89AHgGz3gUm9lZBAXNk2HwB0N";
+//     var baseUrl = "api.tomtom.com"
+//     var versionNumber = 4;
+//     var style = "absolute";
+//     var zoom = 10;
+//     var format = "json";
+//     var point = "33.03,-93.15";
+//     var urlKey = `https://${baseUrl}/traffic/services/${versionNumber}/flowSegmentData/${style}/${zoom}/${format}?key=${apiKey}&point=${point}`//&unit={unit}&thickness={thickness}&openLr={boolean}&jsonp={jsonp}
 
-    $.ajax({
-        url: urlKey, success: function (result) {
-            console.log (result)
-        }
-    });
-
-
+//     $.ajax({
+//         url: urlKey, success: function (result) {
+//             console.log (result)
+//         }
+//     });
 
 
 
 
-
-
-})
+// })
 // >>>>>>> 4f7722c83b1c17184fcda6e0409fec2e71a7d2d5
