b988cd605888912d602c8782ec279f0c9793e298
Fernando Martinez Jr <fernando.martinez.jr.59@gmail.com>
2022-04-14 17:50:52 -0400

    form works and and added ids to html

--- assets/js/tomtom.js
+++ assets/js/tomtom.js
@@ -1,6 +1,7 @@
-var trafficMapEL = document.querySelector("#traffic-map")
-
-
+var trafficMapEL = document.querySelector("#traffic-map");
+var trafficMapBtn = document.querySelector("#toggle-traffic-button");
+var searchFormEl = document.querySelector("#search-form");
+var searchFormInput = document.querySelector("#search-input");
 //tomtom perameters
 const TomBUrl = "https://api.tomtom.com/";
 const TApiKey = "I28sS2O89AHgGz3gUm9lZBAXNk2HwB0N";
@@ -13,3 +14,25 @@ const tileSize = 256;
 var x = 2044;
 var y = 1360;
 let trafficData = fetch(`${TomBUrl}traffic/map/${versionNumber}/tile/flow/${style}/${zoom}/${x}/${y}.${format}?key=${TApiKey}`)
+
+
+function handleFormSubmit(evt) {
+    evt.preventDefault();
+        window.alert("foo");
+  }
+  
+  function handleButtonClick(evt) {
+    
+  }
+
+function addEventListeners() {
+    searchFormEl.addEventListener("submit", handleFormSubmit);
+    // buttonContainerEl.addEventListener("click", handleButtonClick);
+}
+
+function init() {
+    addEventListeners(); 
+    // populateButtons();
+  }
+
+init();
